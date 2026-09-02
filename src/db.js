099184adacdf1f522a3c172f5141dc39bc0ab4f9
Muhammad Jundi Al-Aqsha <muhammadjunnndi@gmail.com>
2026-09-02 13:46:59 +0700

    dbjs

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
+
+export const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port:Number(process.env.DB_PORT) || 8889,
+    user:process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+});
